d1487cd1eafcceb146bb3d4e4e0ccfd2754fcdf1
jax777 <1372736942@qq.com>
2016-04-20 15:33:52 +0800

    Create require.sh

---
+++ require.sh
@@ -0,0 +1,11 @@
+apt-get install -y python-dev
+apt-get install -y nmap
+pip install paramiko
+apt-get install -y python2.7-mysqldb
+pip install dnspython
+pip install pymongo
+pip install python-nmap
+apt-get install -y freetds-dev
+pip install pymssql
+pip install redis
+pip install python-memcached
